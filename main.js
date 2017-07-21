39d9a68425d759ced8e5cdca3471f7fda7dbb3ff
Nimit Sachdeva <nimitsachdeva13@gmail.com>
2017-07-21 09:47:24 +0530

    Start of the project

---
+++ main.js
@@ -0,0 +1,10 @@
+// // Runs when HTML file is loaded
+// $(document).ready(function() {
+//   // $('#user-email').on('keypress',function() {
+//       $('#user-email').on('input',function() {
+//           console.log($('#user-email').val());
+//           var email = $('#user-email').val();
+//           var message = 'Welcome, ' + email;
+//           $('.welcome-message').text(message);
+//   });
+// });
